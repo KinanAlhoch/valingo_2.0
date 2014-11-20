b22a35c974000dcdc39a057899ebb303ff6de3d7
Zia <bigintech@bigintech-HP-Compaq-6910p.(none)>
2014-11-21 00:14:19 +0500

    Pals added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,4 +16,22 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :city, :state, :country)
+  end
 end
