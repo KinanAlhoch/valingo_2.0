58ccffd79511e1c8900f3ea3436d15b914c78e39
Zia <bigintech@bigintech-HP-Compaq-6910p.(none)>
2015-01-21 22:59:45 +0500

    first commit

--- app/assets/javascripts/video-tok.js
+++ app/assets/javascripts/video-tok.js
@@ -2,11 +2,9 @@ $(document).ready(function() {
   /* Activating Best In Place */
   jQuery(".best_in_place").best_in_place();
 });
-
-
-var apiKey = "45115102";
-var sessionId = "2_MX40NTExNTEwMn5-MTQxOTQ0NzMzMjUzNH4ybjZZajNEeTJBM0c5UjRRMWYwSG55UFV-fg";
-var token = "T1==cGFydG5lcl9pZD00NTExNTEwMiZzaWc9ZDQ5NzM5YmMwZmM1MWE1ZmEwMWM3ZDE4MGQ2OWYzYmIyMzI0MmZhMzpyb2xlPXB1Ymxpc2hlciZzZXNzaW9uX2lkPTJfTVg0ME5URXhOVEV3TW41LU1UUXhPVFEwTnpNek1qVXpOSDR5YmpaWmFqTkVlVEpCTTBjNVVqUlJNV1l3U0c1NVVGVi1mZyZjcmVhdGVfdGltZT0xNDE5NDQ3Mzc2Jm5vbmNlPTAuNDIyMDIwMzE5MzA3MTY5Mg==";
+var apiKey = "45135002";
+var sessionId = "2_MX40NTEzNTAwMn5-MTQyMTg2MjU0NTEwM35KWmoxR0c5MXZieTRtUGREc1lKRHExSmt-fg";
+var token = "T1==cGFydG5lcl9pZD00NTEzNTAwMiZzaWc9OGMwNTM3YzE2OTI0YmI5OGU5YTEwYTVmZDAxM2E3M2QwZDU2MDA1Zjpyb2xlPXB1Ymxpc2hlciZzZXNzaW9uX2lkPTJfTVg0ME5URXpOVEF3TW41LU1UUXlNVGcyTWpVME5URXdNMzVLV21veFIwYzVNWFppZVRSdFVHUkVjMWxLUkhFeFNtdC1mZyZjcmVhdGVfdGltZT0xNDIxODYyNjY5Jm5vbmNlPTAuNzY0NjY2Nzc3ODM3Mzg0Ng==";
 
 // Initialize session, set up event listeners, and connect
 var session = OT.initSession(apiKey, sessionId);
